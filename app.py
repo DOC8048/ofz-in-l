e636d61bf5019eafb0e252ff76dbba97425fc83e
DOC8048 <nikita.aydov@gmail.com>
2026-08-13 19:13:21 +0300

    The debugging code is disabled.

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ from datetime import datetime
 # загружаем из модуля new_function
 from function.API_in_function import get_deposit_rates
 from utilits_app.export_utils import generate_excel
-import requests
-try:
-    response = requests.get("https://cbr.ru/secinfo/secinfo.asmx", timeout=5)
-    st.write("Сайт ЦБ доступен")
-except:
-    st.write("Сайт ЦБ недоступен")
+# ======= Отладочный код =======
+# import requests
+# try:
+#     response = requests.get("https://cbr.ru/secinfo/secinfo.asmx", timeout=5)
+#     st.write("Сайт ЦБ доступен")
+# except:
+#     st.write("Сайт ЦБ недоступен")
 @st.cache_data(ttl=14400)
 def get_target_inflation() -> float | None:
     """
